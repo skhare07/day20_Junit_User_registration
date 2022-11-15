40214f58855c5a76392ee61d04df9b579029be84
Sourav <souravkhare34@gmail.com>
2022-11-15 19:51:45 +0530

    Test for last name validation program

--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -3,9 +3,16 @@ package com.bridgelabz.junit_user_registration;
 import org.junit.Test;
 
 public class UserRegistrationTest {
+
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        userRegistration.validateFirstName("Karthik");
+        userRegistration.validateFirstName("Sourav");
+    }
+
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        userRegistration.validateLastName("Khare");
     }
 }
