27c3f5309c9abd7583dd0b925767a67ccdd5762f
Sourav <souravkhare34@gmail.com>
2022-11-15 19:51:26 +0530

    Validate Last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,10 +4,15 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+    private static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
 
     public boolean validateFirstName(String fname) {
-        Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
-        return pattern.matches(FIRST_NAME_PATTERN, fname);
+        Pattern pattern = Pattern.compile(NAME_PATTERN);
+        return pattern.matches(NAME_PATTERN,fname);
+    }
+
+    public boolean validateLastName(String lName) {
+        Pattern pattern = Pattern.compile(NAME_PATTERN);
+        return pattern.matches(NAME_PATTERN,lName);
     }
 }
