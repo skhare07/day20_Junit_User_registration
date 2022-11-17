10ae92ad21802c2f2e59b5be1b9e6c86a7b7ceb8
Sourav <souravkhare34@gmail.com>
2022-11-17 11:04:09 +0530

    Validate email

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
     private static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+    private static final String EMAIL_PATTERN = "^[a-z]{1,10}[._+-]?[a-z]{1,10}?[\\@][a-z]{1,10}[\\.][a-z]{1,4}[\\.][a-z]{2}$";
 
     public boolean validateFirstName(String fname) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
@@ -14,5 +15,10 @@ public class UserRegistration {
     public boolean validateLastName(String lName) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
         return pattern.matches(NAME_PATTERN,lName);
+    }
+
+    public boolean validateEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        return pattern.matches(EMAIL_PATTERN,email);
     }
 }
