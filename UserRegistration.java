e38d5a1a3d5a9f454e6c389c4485a05762e76007
Sourav <souravkhare34@gmail.com>
2022-11-17 11:25:31 +0530

    Validate Phone Number

--- UserRegistration.java
+++ UserRegistration.java
@@ -6,19 +6,25 @@ public class UserRegistration {
 
     private static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
     private static final String EMAIL_PATTERN = "^[a-z]{1,10}[._+-]?[a-z]{1,10}?[\\@][a-z]{1,10}[\\.][a-z]{1,4}[\\.][a-z]{2}$";
+    private static final String PHONE_PATTERN = "^[9]{1}[1]{1}[ ]?[6-9]{1}[0-9]{9}$";
 
     public boolean validateFirstName(String fname) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
-        return pattern.matches(NAME_PATTERN,fname);
+        return pattern.matches(NAME_PATTERN, fname);
     }
 
     public boolean validateLastName(String lName) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
-        return pattern.matches(NAME_PATTERN,lName);
+        return pattern.matches(NAME_PATTERN, lName);
     }
 
     public boolean validateEmail(String email) {
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-        return pattern.matches(EMAIL_PATTERN,email);
+        return pattern.matches(EMAIL_PATTERN, email);
+    }
+
+    public boolean validatePhoneNumber(String phNumber) {
+        Pattern pattern = Pattern.compile(PHONE_PATTERN);
+        return pattern.matches(PHONE_PATTERN, phNumber);
     }
 }
