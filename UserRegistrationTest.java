ee4f75a929524c1db87f6206acf8e9ef632735b0
Sourav <souravkhare34@gmail.com>
2022-11-17 11:25:50 +0530

    Test for phoneNumber validation

--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -1,24 +1,36 @@
 package com.bridgelabz.junit_user_registration;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class UserRegistrationTest {
 
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        userRegistration.validateFirstName("Sourav");
-            }
+        boolean fName = userRegistration.validateFirstName("Sourav");
+        Assertions.assertTrue(fName);
+    }
 
     @Test
     public void givenLastName_WhenProper_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        userRegistration.validateLastName("Khare");
+        boolean lName = userRegistration.validateLastName("Khare");
+        Assertions.assertTrue(lName);
     }
 
     @Test
     public void givenEmail_WhenProper_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        userRegistration.validateEmail("abc.xyz@bl.co.in");
+        boolean email = userRegistration.validateEmail("abc.xyz@bl.co.in");
+        Assertions.assertTrue(email);
     }
+
+    @Test
+    public void givenPhoneNumber_WhenProperWithSpace_ShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean phoneNumber = userRegistration.validatePhoneNumber("91 9876543210");
+        Assertions.assertTrue(phoneNumber);
+    }
+
 }
